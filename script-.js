6b3c50ee1b657c76a0e4717577a14a70af03b641
ByteBloom <trotta.mara@gmail.com>
2024-09-10 21:16:05 +0200

    Update script-.js
    Aggiunte tutte le funzionalità per la taverna, compreso un gioco di tiradadi con l'Oste

--- script-.js
+++ script-.js
@@ -1,7 +1,7 @@
 const personaggio = {
     livello: 1,
     xp: 0,
-    salute: 100 ,
+    salute: 100,
     maxVita: 100,
     difesa: 0,
     forza: 0,
@@ -13,6 +13,10 @@ const personaggio = {
         pettorale: null,
         gambali: null,
         amuleto: null
+    },
+    flag: {
+        sentieroArma: false,
+        osteArma: false
     }
 };
 
@@ -34,7 +38,7 @@ const stati = [
             "poter raggiungere il bancone. Dietro di esso c'è una donna dalla pelle incartapecorita, china su se stessa: la sua età è indefinibile, " +
             "ma ha lo sguardo buono.",
         bottoni: ["Torna in città", "Chiacchiera"],
-        funzioni: [goCitta, chiacchiera]
+        funzioni: [goCitta, chiacchieraBottega]
     },
     {
         nome: "chiacchiera",
@@ -42,9 +46,39 @@ const stati = [
             "rilevanti da condividere.",
         bottoni: ["Continua a guardare nella bottega", "Torna in città"],
         funzioni: [goNegozio, goCitta]
+    },
+    {
+        nome: "taverna",
+        text: "Entri in quella che sembra la tipica taverna di un tipico villaggio di quella regione: accogliente e calda. C'è un solo problema: è fin " +
+            "pulita e oltre all'oste non sembra esserci nessuno, eppure l'orario non è insolito per frequentare un posto del genere.",
+        bottoni: ["Rivolgiti all'oste", "Torna in città"],
+        funzioni: [goOste, goCitta]
+    },
+    {
+        nome: "oste",
+        text: "L'oste è un anziano dalla barba importante, dall'aspetto burbero e dall'aria preoccupata. Ti accoglie con un cenno del capo e con un gesto " +
+            "ti invita a sederti allo sgabello di fronte a lui, al bancone.\n 'Gli affari qui vanno sempre peggio, da quando quel drago ha deciso di stabilirsi "+
+            "sulla montagna. La gente del villaggio è troppo in pena per concedersi una bevuta e il denaro e le risorse iniziano a scarseggiare: quel mostro " +
+            "si nutre del nostro bestiame e dà fuoco ai nostri raccolti. Sei il primo viandante che si vede da queste parti dopo molto tempo, perchè l'unica " +
+            " strada per arrivare qui passa dalla montagna. Non so per quanto tempo riusciremo a resistere'",
+        bottoni: ["Chiacchiera", "Gioca", "Torna in città"],
+        funzioni: [chiacchieraOste, giocoOste, goCitta]
+    },
+    {
+        nome: "tiraDadi",
+        text: "L'oste tira fuori due coppie di dadi e te ne porge una. 'Ho qui alcuni dadi, giochiamo per passare il tempo? Il gioco è semplice: punti "+
+            "cinque monete, poi entrambi lanciamo i dadi, se fai il mio stesso punteggio ti restituisco la puntata ne vinci sessanta. Ti va?'",
+        bottoni: ["Tira i dadi", "Declina l'invito", "Torna in città"],
+        funzioni: [lanciaDadi, goOste, goCitta]
+    },
+    {
+        nome: "sentiero",
+        text: "Ti lasci il villaggio alle spalle e ti incammini lungo il sentiero fin quando non raggiungi un trivio. Dove decidi di andare?",
+        bottoni: ["Vai verso la caverna", "Vai verso il bosco", "Prosegui verso la montagna", "Torna al villaggio"],
+        funzioni: [goCaverna, goBosco, goMontagna, goCitta]
     }
 ];
-
+console.log(stati);
 let statoCorrente = stati[0]; // Stato iniziale
 let itemName;
 
@@ -105,8 +139,10 @@ function aggiornaStato(stato) {
     statoCorrente = stati.find(tmp => tmp.nome === stato.nome);
     console.log(statoCorrente.nome);
     testo.innerText = stato.text;
+    console.log(stato.text);
 
     const index = stati.findIndex(elemento => elemento.nome === stato.nome);
+    console.log(index);
     immagine.src = `./res/img/${index}.webp`;
 
     aggiornaBottoni();
@@ -122,16 +158,121 @@ function goCitta() {
 function goNegozio() {
     aggiornaStato(stati[1]);
     mostraDivBottega(true);
-    mostraDivMostro(false);
     riempiInventario();
 }
 
-function chiacchiera() {
+function chiacchieraBottega() {
     aggiornaStato(stati[2]);
     mostraDivBottega(false);
-    mostraDivMostro(false);
     riempiInventario();
 }
+
+function goTaverna() {
+    aggiornaStato(stati[3]);
+}
+
+function goOste(){
+    aggiornaStato(stati[4]);
+}
+
+function chiacchieraOste(){
+    let str= "'Fai attenzione ad avventurarti fuori di qua. Il sentiero che conduce verso l'esterno del villaggio si divide in tre: "+
+        "puoi deviare verso la caverna, verso il bosco o andare diretto alla montagna. La caverna è infestata da ragni giganti e dagli "+
+        "scheletri non morti dei poveri avventurieri incauti che ci hanno lasciato le penne. Il bosco è abitato da strani cinghiali "+
+        "particolarmente grossi e aggressivi e dai banditi, ma il pericolo più grande sono i grifoni.'"
+    const strArma= "L'oste fa una breve pausa in grave silenzio, poi solleva le sopracciglia e si affretta ad afferrare qualcosa "+
+        "da sotto il bancone. Ti porge un pugnale 'Sono sicuro che questo sarà più utile a te che a me, prendilo pure'."
+    
+    //se non hai già chiacchierato con l'oste
+    if(!document.getElementById("messaggio")){
+        const div= document.createElement("div");
+        div.setAttribute("id", "messaggio");
+        div.textContent=str;
+        testo.appendChild(div);
+
+        //se l'oste non ti ha mai dato l'arma
+        if (!personaggio.flag.osteArma){
+            personaggio.flag.osteArma=true;
+            personaggio.inventario.push(tuttiGliOggetti.find(oggetto => oggetto.nome === "pugnale dell'Oste"));
+            riempiInventario();
+            div.appendChild(document.createElement("br"));
+            div.appendChild(document.createElement("br"));
+            const span= document.createElement("span");
+            span.textContent=strArma;
+            div.appendChild(span);
+        }
+    }
+    
+}
+
+function giocoOste(){
+    aggiornaStato(stati[5]);
+}
+
+function lanciaDadi(){
+    
+    console.log("click tiradadi")
+    let div;
+    //se non hai già giocato, crea il div
+    if(!document.getElementById("messaggio")){
+        div= document.createElement("div");
+        div.setAttribute("id", "messaggio");
+        testo.appendChild(div);
+    }
+    //altrimenti svuota il contenuto
+    else{
+        div= document.getElementById("messaggio");
+        div.innerHTML="";
+    }
+
+    //se il personaggio non ha abbastanza monete
+    if(personaggio.oro <5){
+        div.innerText="Sembra che tu non abbia abbastanza monete per giocare. Facciamo un'altra volta, eh?"
+    }else{
+        //se il personaggio può giocare
+        const puntiOste= tiraDadi();
+        const puntiPg = tiraDadi()
+        const tiroOste="L'oste tira i dadi e totalizza "+ puntiOste + " punti.";
+        const tiroPg= "Tu tiri i dadi e totalizzi "+ puntiPg + " punti";
+        let spanOste=document.createElement("span");
+        spanOste.textContent=tiroOste;
+        div.appendChild(spanOste);
+        div.appendChild(document.createElement("br"));
+        div.appendChild(document.createElement("br"));
+        let spanPg=document.createElement("span");;
+        spanPg.textContent=tiroPg;
+        div.appendChild(spanPg);
+        div.appendChild(document.createElement("br"));
+        div.appendChild(document.createElement("br"));
+        let spanRisultati=document.createElement("span");
+        if(puntiPg===puntiOste){
+            personaggio.oro+=60;
+            spanRisultati.textContent= "L'oste scuote la testa e sbuffa mentre ti consegna la vincita. 'Sembra proprio che tu abbia vinto, questa volta'";
+        }
+        else{
+            personaggio.oro-=5;
+            spanRisultati.textContent= "L'oste si stringe nelle spalle incassando le monete. 'Ti andrà meglio la prossima volta... forse.'";
+        }
+        aggiornaStatistichePersonaggio();
+        div.appendChild(spanRisultati);
+    }
+}
+
+function goSentiero() {
+    aggiornaStato(stati[6]);
+    //se non hai avuto l'arma dall'oste e non hai trovato prima il bastone
+    if (!personaggio.flag.osteArma && !personaggio.flag.sentieroArma){
+        const div= document.createElement("div");
+        div.setAttribute("id", "messaggio");
+        div.textContent="Prima di recarti da incamminarti, rifletti che è molto meglio avere un'arma qualsiasi piuttosto che avventurarti "+
+            "verso posti sconosciuti a mani nude. Cercando in giro trovi un bastone e decidi che, per il momento, può andare";
+        testo.appendChild(div);
+        personaggio.inventario.push(tuttiGliOggetti.find(oggetto => oggetto.nome === "bastone"));
+        riempiInventario();
+        personaggio.flag.sentieroArma=true;
+    }
+}
+
 
 function goCaverna() {
     console.log("click caverna")
@@ -141,7 +282,7 @@ function goBosco() {
     console.log("click bosco")
 }
 
-function goDrago() {
+function goMontagna() {
     console.log("click drago")
 }
 
@@ -149,9 +290,6 @@ function goSentiero() {
     console.log("click sentiero")
 }
 
-function goTaverna() {
-    console.log("click taverna")
-}
 
 
 
@@ -250,13 +388,12 @@ function compra(itemNome) {
 
     // Trova l'oggetto completo basato sul nome
     const item = tuttiGliOggetti.find(oggetto => oggetto.nome === itemNome);
-    
-    if (!item) {
-        console.log("Oggetto non trovato: " + itemNome);
-        return;
-    }
 
     const prezzo = item.prezzoAcquisto;
+
+    let stringOK = "La venditrice ti consegna ciò che hai chiesto, mette via il denaro ricevuto e ti sorride: 'Posso esserti ancora utile?'";
+    let stringWarning= "La venditrice ti guarda con aria desolata, inarca debolmente l'interno delle sopracciglia verso l'alto e scuote la testa: " +
+        "'Mi dispiace, ma sembra che tu non possa permetterti quest'oggetto.'"
 
     if (prezzo <= personaggio.oro) {
         // Aggiunge l'oggetto all'inventario e aggiorna i dati del personaggio
@@ -265,16 +402,17 @@ function compra(itemNome) {
         riempiInventario();
         aggiornaStatistichePersonaggio();
 
-        //se esiste un precedente messaggio di ok, mostralo di nuovo
+        
+        //se esiste un precedente messaggio di ok, mostralo di nuovo e aggiorna il testo
         if(ok){
             console.log("ok esiste");
-            ok.textContent = "La venditrice ti consegna ciò che hai chiesto, mette via il denaro ricevuto e ti sorride: 'Posso esserti ancora utile?'";
+            ok.textContent = stringOK;
             ok.style.display='block';
         } else{
             // Crea il messaggio di errore
             const div = document.createElement("div");
             div.classList.add("ok");
-            div.textContent = "La venditrice ti consegna ciò che hai chiesto, mette via il denaro ricevuto e ti sorride: 'Posso esserti ancora utile?'";
+            div.textContent = stringOK;
             testo.appendChild(div); // Aggiunge il messaggio al contenitore `testo`
         }
     } else {
@@ -286,14 +424,45 @@ function compra(itemNome) {
             // Crea il messaggio di errore
             const div = document.createElement("div");
             div.classList.add("warning");
-            div.textContent = "La venditrice ti guarda con aria desolata, inarca debolmente l'interno delle sopracciglia verso l'alto e scuote la testa: " +
-                "'Mi dispiace, ma sembra che tu non possa permetterti quest'oggetto.'";
+            div.textContent = stringWarning;
             testo.appendChild(div); // Aggiunge il messaggio al contenitore `testo`
         }
     }
 }
 
-function vendi() {
+function vendi(itemNome) {
+    // Trova l'oggetto completo basato sul nome
+    const item = tuttiGliOggetti.find(oggetto => oggetto.nome === itemNome);
+    const prezzo= item.prezzoVendita;
+
+    let ok;
+    let stringOK= "La venditrice ti consegna il denaro e prende l'oggetto appena acquistato: 'Sono sicura che prima o poi riusciremo "+
+        "a dargli una nuova vita'. Ti serve altro?";
+
+    if(document.querySelector(".ok")){
+        ok= document.querySelector(".ok");
+        ok.style.display= 'none';
+    }
+
+    //vendi l'Item e aggiorna statistiche e inventario
+    personaggio.oro+=prezzo;
+    rimuoviItem(personaggio.inventario, item);
+    aggiornaStatistichePersonaggio();
+    riempiInventario();
+
+    
+    //se esiste un precedente messaggio di ok, mostralo di nuovo e aggiorna il testo
+    if(ok){
+        ok.textContent = stringOK;
+        ok.style.display='block';
+    } else{
+        // Crea il messaggio di errore
+        const div = document.createElement("div");
+        div.classList.add("ok");
+        div.textContent = stringOK;
+        testo.appendChild(div); // Aggiunge il messaggio al contenitore `testo`
+    }
+
 
 }
 
@@ -339,56 +508,56 @@ function showItemMini(item) {
         '<span class="descrizioneItem">' + descrizione + '</span><br/>';
 }
 
-    function riempiInventario() {
-    
-        // Messaggio quando l'inventario è vuoto
-        if (personaggio.inventario.length === 0) {
-            equip.innerText = "Non hai equipaggiamento, dovresti passare al negozio a comprare qualcosa ed equipaggiare il necessario.";
-            return;
-        }
-    
-        // Puliamo il contenuto di `borsa` prima di riempirlo
-        borsa.innerHTML = "";
-    
-        personaggio.inventario.forEach(item => {
-            // Cerchiamo l'oggetto nell'inventario
-            const oggetto = tuttiGliOggetti.find(o => o.nome === item.nome);
-    
-            if (oggetto) {
-                // Creiamo un div che conterrà l'elemento visualizzato e il bottone
-                const itemContainer = document.createElement("div");
-                itemContainer.classList.add("itemMini");
-    
-                // Utilizziamo `showItemMini` per visualizzare l'oggetto
-                itemContainer.innerHTML += showItemMini(oggetto);
-    
-                // Creiamo il bottone per "Usa" o "Equipaggia"
-                const button = document.createElement("button");
-                button.innerText = item.tipo === 'pozione' ? "Usa" : "Equipaggia";
-                button.addEventListener('click', () => {
-                    if (item.tipo === 'pozione') {
-                        usa(item.nome);
-                    } else {
-                        equipaggia(item.nome);
-                    }
-                });
-    
-                // Aggiungiamo il bottone al container dell'oggetto
-                itemContainer.appendChild(button);
-    
-                // Verifica se siamo nello stato "bottega" o "chiacchiera"
-                if (isStatoCorrente(stati[1])) {
-                    const sellButton = document.createElement("button");
-                    sellButton.textContent = `Vendi a ${oggetto.prezzoVendita}`;
-                    sellButton.addEventListener('click', () => vendi(item.nome));
-                    itemContainer.appendChild(sellButton);
-                }
-    
-                // Appendiamo il contenitore dell'elemento a `borsa`
-                borsa.appendChild(itemContainer);
-            }
-        });
+function riempiInventario() {
+
+    // Messaggio quando l'inventario è vuoto
+    if (personaggio.inventario.length === 0) {
+        borsa.innerText = "Non hai nulla nella borsa, potresti passare in bottega a comprare qualcosa.";
+        return;
     }
+
+    // Puliamo il contenuto di `borsa` prima di riempirlo
+    borsa.innerHTML = "";
+
+    personaggio.inventario.forEach(item => {
+        // Cerchiamo l'oggetto nell'inventario
+        const oggetto = tuttiGliOggetti.find(o => o.nome === item.nome);
+
+        if (oggetto) {
+            // Creiamo un div che conterrà l'elemento visualizzato e il bottone
+            const itemContainer = document.createElement("div");
+            itemContainer.classList.add("itemMini");
+
+            // Utilizziamo `showItemMini` per visualizzare l'oggetto
+            itemContainer.innerHTML += showItemMini(oggetto);
+
+            // Creiamo il bottone per "Usa" o "Equipaggia"
+            const button = document.createElement("button");
+            button.innerText = item.tipo === 'pozione' ? "Usa" : "Equipaggia";
+            button.addEventListener('click', () => {
+                if (item.tipo === 'pozione') {
+                    usa(item.nome);
+                } else {
+                    equipaggia(item.nome);
+                }
+            });
+
+            // Aggiungiamo il bottone al container dell'oggetto
+            itemContainer.appendChild(button);
+
+            // Verifica se siamo nello stato "bottega" o "chiacchiera"
+            if (isStatoCorrente(stati[1])) {
+                const sellButton = document.createElement("button");
+                sellButton.textContent = `Vendi a ${oggetto.prezzoVendita}`;
+                sellButton.addEventListener('click', () => vendi(item.nome));
+                itemContainer.appendChild(sellButton);
+            }
+
+            // Appendiamo il contenitore dell'elemento a `borsa`
+            borsa.appendChild(itemContainer);
+        }
+    });
+}
 
 function riempiEquipaggiamento() {
     let str = "";
@@ -444,23 +613,25 @@ function riempiNegozio() {
 
         //itero gli oggetti della categoria e li mostro
         categorieItem[category].forEach(item => {
+            //se l'Item è venduto dalla bottega
+            if(item.bottega){
+                //creo il div dell'item
+                let singleItem = document.createElement('div');
+                singleItem.classList.add("negozioItem");
 
-            //creo il div dell'item
-            let singleItem = document.createElement('div');
-            singleItem.classList.add("negozioItem");
+                //agiungo la descrizione dell'oggetto e creo il pulsante per acquistarlo
+                singleItem.innerHTML += showItemMini(item);
+                const center = document.createElement('div');
+                center.classList.add("centra");
+                const button = document.createElement('button');
+                button.innerText = "Compra a " + item.prezzoAcquisto;
+                button.addEventListener('click', () => compra(item.nome));
+                center.appendChild(button);
+                singleItem.appendChild(center);
 
-            //agiungo la descrizione dell'oggetto e creo il pulsante per acquistarlo
-            singleItem.innerHTML += showItemMini(item);
-            const center = document.createElement('div');
-            center.classList.add("centra");
-            const button = document.createElement('button');
-            button.innerText = "Compra a " + item.prezzoAcquisto;
-            button.addEventListener('click', () => compra(item.nome));
-            center.appendChild(button);
-            singleItem.appendChild(center);
-
-            //aggiungo il div dell'oggetto alla sezione
-            categoriaDiv.appendChild(singleItem);
+                //aggiungo il div dell'oggetto alla sezione
+                categoriaDiv.appendChild(singleItem);
+            }
         });
 
     }
@@ -556,3 +727,8 @@ function isStatoCorrente(stato) {
     return stato.nome === statoCorrente.nome;
 }
 
+function tiraDadi() {
+    //restituisce un intero casuale tra 2 e 12
+    return Math.floor(Math.random() * (Math.floor(12) - Math.ceil(2)) + Math.ceil(2));
+  }
+  
